3d93740371f78e252c9bb538ce905af0f090dbc6
Yonas Gebregziabher <ygebregz@bowdoin.edu>
2023-04-09 15:35:02 +0200

    send and recieve message update

--- chat-client.py
+++ chat-client.py
@@ -1,62 +1,85 @@
 import socket
-
+import select
+import sys
 
 HOST_NAME = "143.47.184.219"
 PORT_NUMBER = 5378
+BUFFER_SIZE = 2048
 
 
-def handshake(s):
-    # Connect to another application.
-    s.connect((HOST_NAME, PORT_NUMBER))
-    
-    # Asking for a unique username
+def handshake(socket_instance):
+    socket_instance.connect((HOST_NAME, PORT_NUMBER))
     user_name = input("Please enter a user name: ")
     user_name = f"HELLO-FROM {user_name}\n"
-    name_bytes_sent = s.send(user_name.encode())
-    buffer = s.recv(2048)
+    name_bytes_sent = socket_instance.send(user_name.encode())
+    buffer = socket_instance.recv(BUFFER_SIZE)
     return buffer
 
-def userCommand(s):
-    command = input("Please enter a command: ")
 
-    if(command == "!quit"): # Lets the user shutdown the client by typing !quit
-        return None
-    elif(command == "!who"): #Lets the user list all currently logged-in users by typing !who
+def handle_user_input(command, socket_instance):
+    if (command == "!quit"):  # Lets the user shutdown the client by typing !quit
+        return sys.exit(1)
+    elif (command == "!who"):  # Lets the user list all currently logged-in users by typing !who
         user_list = "LIST\n"
-        list_bytes_sent = s.send(user_list.encode())
-    elif(command.startswith("@")): # Lets the user send messages to other users by typing @username message
+        list_bytes_sent = socket_instance.send(user_list.encode())
+    # Lets the user send messages to other users by typing @username message
+    elif (command.startswith("@")):
         message_list = command.split()
         user_name = message_list[0][1:]
-        message = message_list[1]
+        message = message_list[1].strip("\n")
         send_message = f"SEND {user_name} {message}\n"
-        list_bytes_sent = s.send(send_message.encode())
-    buffer = s.recv(2048)
+        list_bytes_sent = socket_instance.send(send_message.encode())
+    else:
+        return None
+    buffer = socket_instance.recv(BUFFER_SIZE)
     return buffer
 
 
+def server_output_to_msg(server_output):
+    if server_output is None:
+        return "Error with given command or server response."
+    server_output = server_output.decode('ascii')
+    if server_output == "SEND-OK\n":
+        return "Message sent successfully!"
+    elif server_output == "BAD-DEST-USER\n":
+        return "User is currently not online or doesn't exist."
+    elif server_output == "BUSY\n":
+        return "Server is currently busy!"
+    elif server_output == "BAD-RQST-HDR\n":
+        return "Request header is invalid."
+    elif server_output == "BAD-RQST-BODY\n":
+        return "Request body is invalid."
+    elif server_output.startswith("LIST-OK"):
+        return server_output.strip("LIST-OK")
+    elif server_output.startswith("HELLO"):
+        username = server_output.split(" ")
+        return "Welcome online " + username[1]
 
-# Errors still need to be handled
+
 def main():
-    # Create a new socket.
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    user_name = handshake(s) # Response from server is returned
-    while(user_name == b'IN-USE\n'): # This asks for another name if the chosen name is already taken.
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        user_name = handshake(s)
-    print(user_name)
-    
-    # We let the user interact with the server
-    command = userCommand(s) # Response from server is returned and none is user quits
-    while(command != None):
-        print(command)
-        command = userCommand(s)
+    socket_instance = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    user = handshake(socket_instance)
+    while (user == b"IN-USE\n"):
+        print("User name already taken.")
+        socket_instance = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        user = handshake(socket_instance)
+    print("Please always enter your command in the empty new line: ")
+    sockets = [socket_instance, sys.stdin]
+    while True:
+        read_message, write_message, error = select.select(
+            sockets, [], [], 0.5)
+        if error:
+            print(f"Error: {error}")
+        if sys.stdin in read_message:
+            user_command = input()
+            res = handle_user_input(user_command, socket_instance)
+            print(server_output_to_msg(res))
+        for sock in read_message:
+            if sock == socket_instance:
+                msg = sock.recv(BUFFER_SIZE).decode('ascii')
+                if msg.startswith("DELIVERY"):
+                    messages = msg.split(" ")
+                    print("\n" + messages[1] + " --> " + messages[2])
 
-    # This still needs to be worked on, but we should be able to messages from other users and display them to the user
-    # I'm thinking, we need to use threading 
-    # received_message = s.recv(2048)
-    # print(received_message)
-
-    # Close connection.
-    s.close()
 
 main()
